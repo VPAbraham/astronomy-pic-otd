a6f58e69e242a798ad3f1d816ac8408e2287677e
Victor Abraham <vpa4565@gmail.com>
2020-01-14 23:33:16 -0700

    Rename some variables

--- src/functions/helpers.js
+++ src/functions/helpers.js
@@ -1,15 +1,15 @@
 export const getCurrentDate = () => {
   const endDate = Date.now();
-  const startDate = endDate - 2592000000;
-  const newStartDate = new Date(startDate);
-  const startYear = newStartDate.getFullYear().toString();
-  const startMonth = checkDateLength(newStartDate.getMonth() + 1);
-  const startDay = checkDateLength(newStartDate.getDate());
+  const startDateMill = endDate - 2592000000;
+  const startDate = new Date(startDateMill);
+  const startYear = startDate.getFullYear().toString();
+  const startMonth = dateHelper(startDate.getMonth() + 1);
+  const startDay = dateHelper(startDate.getDate());
   const startDateValue = `${startYear}-${startMonth}-${startDay}`;
   return { startDate: startDateValue};
 };
 
-export const checkDateLength = date => {
+export const dateHelper = date => {
   const dateToString = date.toString();
   if (!(dateToString.length > 1)) {
     return "0" + dateToString;
